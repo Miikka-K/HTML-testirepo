8357e26716082e901d37cdf32f51e87769f4003c
Miikka-K <51471@edu.raseko.fi>
2024-03-18 13:53:42 +0200

    Pikku juttuja :D nappi ei toimi

---
+++ text_to_html/converter/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.db.models import Model
+from ckeditor.fields import RichTextField
+
+# Create your models here.
+class Editor(Model):
+    body = RichTextField(blank=True, null=True)
+
---
+++ text_to_html/converter/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .forms import EditorForm
+# Create your views here.
+def index(request):
+    form = EditorForm()
+    return render(request, 'index.html', {'form': form})
